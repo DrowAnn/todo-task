4beb39e4f5cf7fa2df047a4e1b35a7673f362e69
Robert Steven Delgado Peralta <rs19990801@gmail.com>
2024-06-07 20:10:59 -0500

    Estructura de las tareas definida

--- api.js
+++ api.js
@@ -1,8 +1,34 @@
 var mongoose = require("mongoose");
 var express = require("express");
 var router = express.Router();
+var TaskModel = require("./task_schema");
+
+let environment = null;
+
+if (!process.env.ON_HEROKU) {
+  console.log("Cargando variables de entorno desde archivo");
+  const env = require("node-env-file");
+  env(__dirname + "/.env");
+}
+
+environment = {
+  DBMONGOUSER: process.env.DBMONGOUSER,
+  DBMONGOPASS: process.env.DBMONGOPASS,
+  DBMONGOSERV: process.env.DBMONGOSERV,
+  DBMONGO: process.env.DBMONGO,
+};
+
 var query =
-  "mongodb+srv://RobertPruebas:RD123456@pruebas.rk73dn5.mongodb.net/Pruebas?retryWrites=true&w=majority";
+  "mongodb+srv://" +
+  environment.DBMONGOUSER +
+  ":" +
+  environment.DBMONGOPASS +
+  "@" +
+  environment.DBMONGOSERV +
+  "/" +
+  environment.DBMONGO +
+  "?retryWrites=true&w=majority";
+
 const db = query;
 
 mongoose.Promise = global.Promise;
